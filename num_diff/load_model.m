96d4796bcd8b24f40e80d8a65c79d4c05294a556
albanie <albanie@robots.ox.ac.uk>
2017-06-09 10:31:25 +0100

    cleanup

--- num_diff/load_model.m
+++
@@ -1,6 +0,0 @@
-model = 'alexnet-mcn.mat' ;
-modelDir = fullfile(vl_rootnn, 'contrib/mcnPyTorch/models') ;
-net = load(fullfile(modelDir, model)) ;
-
-dag = dagnn.DagNN.loadobj(net) ;
-dag.addLayer('softmax', dagnn.SoftMax(), dag.layers(end).outputs, 'prob', {}) ;
