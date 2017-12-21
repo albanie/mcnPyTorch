dcfa866fecfa8cf4cf17e2719dabc3c7f000b9f2
albanie <albanie@robots.ox.ac.uk>
2017-12-21 09:21:15 +0000

    mods

--- setup_mcnPyTorch.m
+++ setup_mcnPyTorch.m
@@ -1,9 +1,9 @@
 function setup_mcnPyTorch()
-%SETUP_MCNPYTORCH Sets up mcnPyTorch, by adding its folders 
+%SETUP_MCNPYTORCH Sets up mcnPyTorch, by adding its folders
 % to the Matlab path
 %
 % Licensed under The MIT License [see LICENSE.md for details]
-% Copyright (C) 2017 Samuel Albanie 
+% Copyright (C) 2017 Samuel Albanie
 
   root = fileparts(mfilename('fullpath')) ;
   addpath(root, [root '/test'], [root '/benchmarks']) ;
--- test/compare_mcn_features.m
+++ test/compare_mcn_features.m
@@ -1,31 +1,30 @@
 function compare_mcn_features
 % COMPARE_MCN_FEATURES - check mcn vs pytorch features
-% 
+%
 %   NOTE: Pytorch does not support "same" style padding. As a result,
 %   it is difficult to reproduce features exactly that use zero padding
-%   without a lot of extra computation (i.e. actively inserting 
+%   without a lot of extra computation (i.e. actively inserting
 %   additional zeros into feature maps). If the scores are OK without
 %   modifying the architecture, it's easier to leave it as it is.
-% 
+%
 % mcnPyTorch
 % Licensed under The MIT License [see LICENSE.md for details]
-% Copyright (C) 2017 Samuel Albanie 
+% Copyright (C) 2017 Samuel Albanie
 
   opts.featurePath = 'data/mcnPyTorch/pyt-feats.mat' ;
   opts.modelDir = fullfile(vl_rootnn, 'data/models-import') ;
-  opts.modelPath = fullfile(opts.modelDir, 'densenet121-pt-mcn.mat') ;
+  opts.modelPath = 'contrib/mcnPyTorch/models/resnet50-pt-mcn.mat' ;
 
-  feats = load(opts.featurePath) ; 
+  feats = load(opts.featurePath) ;
   dag = dagnn.DagNN.loadobj(load(opts.modelPath)) ;
 
   % reshape input image
   im = permute(feats.x0, [3 4 2 1]) ;
   data = im(:,:,[1 2 3]) ;
 
-  dag.conserveMemory = 0 ; 
+  dag.conserveMemory = 0 ;
   dag.mode = 'test' ;
   dag.eval({'data', data}) ;
-
 
   for ii = 1:numel(dag.vars) - 2
     xName = dag.vars(ii).name ;
